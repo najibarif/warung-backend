7200602e18e986a407a78c64a56b45dc466a05cb
najibarif <nnajiba1357@gmail.com>
2026-06-24 15:43:24 +0700

    chore(security): remove temporary migrations endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,6 @@ Route::get('/ping', function () {
     return response()->json(['pong' => true]);
 });
 
-Route::get('/run-migrations', function () {
-    try {
-        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        return response()->json([
-            'success' => true,
-            'output' => \Illuminate\Support\Facades\Artisan::output()
-        ]);
-    } catch (\Exception $e) {
-        return response()->json([
-            'success' => false,
-            'error' => $e->getMessage()
-        ], 500);
-    }
-});
-
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\FavoriteController;
